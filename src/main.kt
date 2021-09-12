b6ff30b66714e44f0919c59f32b5e251647a726d
jmcmahon38 <jmcmahon38@students.mchenry.edu>
2021-09-12 11:22:15 -0500

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,25 @@
+
+
+
+
+fun main(args: Array<String>) {
+
+
+    var myBit: Byte = 8
+    var myShort: Short = 300
+    var myInt: Int = 10
+    var myLong = 40L
+    var myFloat: Float = 3.14f
+    var myDouble: Double = 1.23456789
+    var myBool: Boolean = true
+    var myChar: Char = '\$'
+
+    println("bit: $myBit, \n" +
+            "short: $myShort \n" +
+            "int: $myInt, \n" +
+            "long: $myLong, \n" +
+            "float: $myFloat, \n" +
+            "double: $myDouble, \n" +
+            "bool: $myBool, \n" +
+            "char: $myChar")
+}
