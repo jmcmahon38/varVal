ab9c9be877a005b14b456261ee786df050bb9669
jmcmahon38 <jmcmahon38@students.mchenry.edu>
2021-09-16 19:14:25 -0500

    removed commas in the println arguments

--- src/main.kt
+++ src/main.kt
@@ -4,22 +4,21 @@
 
 fun main(args: Array<String>) {
 
+    val myBit: Byte = 8
+    val myShort: Short = 300
+    val myInt: Int = 10
+    val myLong = 40L
+    val myFloat: Float = 3.14f
+    val myDouble: Double = 1.23456789
+    val myBool: Boolean = true
+    val myChar: Char = '\$'
 
-    var myBit: Byte = 8
-    var myShort: Short = 300
-    var myInt: Int = 10
-    var myLong = 40L
-    var myFloat: Float = 3.14f
-    var myDouble: Double = 1.23456789
-    var myBool: Boolean = true
-    var myChar: Char = '\$'
-
-    println("bit: $myBit, \n" +
+    println("bit: $myBit \n" +
             "short: $myShort \n" +
-            "int: $myInt, \n" +
-            "long: $myLong, \n" +
-            "float: $myFloat, \n" +
-            "double: $myDouble, \n" +
-            "bool: $myBool, \n" +
+            "int: $myInt \n" +
+            "long: $myLong \n" +
+            "float: $myFloat \n" +
+            "double: $myDouble \n" +
+            "bool: $myBool \n" +
             "char: $myChar")
 }
